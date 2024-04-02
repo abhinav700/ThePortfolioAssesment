7fd8f99c806d9b9e7fad32124eca408d129f9141
abhinav agarwal <abhinavagrawal700@gmail.com>
2024-04-02 12:10:16 +0530

    Added responsiveness

--- src/components/ServicesSection/ServicesSection.tsx
+++ src/components/ServicesSection/ServicesSection.tsx
@@ -6,8 +6,8 @@ const ServicesSection = ({ servicesData }: any) => {
   const mappedArray = servicesData.map((item: any, index: any) => {
     return (
       <Carousel.Item key={item._id}>
-        <div className="mx-auto w-[900px] flex justify-between items-center h-[300px]">
-          <div className="w-1/2">
+        <div className="mx-auto max-w-[900px] flex flex-col lg:flex-row justify-between items-start lg:items-center lg:h-[300px]  h-[300px]">
+          <div className="lg:w-1/2 lg:block hidden">
             <Image
               alt={item.name}
               src={item.image.url}
@@ -15,9 +15,9 @@ const ServicesSection = ({ servicesData }: any) => {
               width={250}
             />
           </div>
-          <div className="bg-black w-full text-white p-4">
-              <h1 className="my-3">{item.name}</h1>
-              <h4 className="my-3">{item.desc}</h4>
+          <div className="bg-black w-full text-white p-4 ">
+              <h1 className="lg:my-3 whitespace-normal">{item.name}</h1>
+              <h4 className="my-3 break-words">{item.desc}</h4>
               <div className="flex flex-wrap items-center"><h4 className="my-3 mr-3">Starting Price: </h4> <h3 className="my-3">{item.charge}</h3></div>
           </div>
         </div>
@@ -25,11 +25,11 @@ const ServicesSection = ({ servicesData }: any) => {
     );
   });
   return (
-    <div className="my-4 ">
+    <div className="my-7 ">
       <h1 className="text-3xl md:text-5xl text-center my-12 font-bold text-black font-serif">
         SERVICES PROVIDED
       </h1>
-      <div className="bg-black w-full">
+      <div className="bg-black w-full my-4">
 
       <Carousel>{mappedArray}</Carousel>
       </div>
